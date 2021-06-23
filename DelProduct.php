743bfbbc0c475869f45f729dbb1054c3ab090c8b
BondDoan <68988327+Doanhuutrung@users.noreply.github.com>
2021-06-23 12:48:06 +0700

    Add files via upload

---
+++ DelProduct.php
@@ -0,0 +1,27 @@
+<?php
+   $connect = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com
+   dbname=d7fnvuie08bqea
+   port=5432
+   user=qbwkglenipljgh
+   password=bd2de50a1827c44d273e78356a3fca0f849403100e1a4b5f12d1c91f91a7e9ee
+    sslmode=require");
+     if ($connect === false) {
+      die("ERROR: Something went wrong with connection!");
+    } else {
+      $product_name = $_POST['Productname'];
+      $product_type = $_POST['Type'];
+      $price = $_POST['Price'];
+
+    }
+    $query = "DELETE FROM product (product_name, product_type, price) 
+    VALUES('$product_name', '$product_type', '$price');";
+    $result = pg_query($connect, $query);
+    if ($result) {
+      echo "<script>alert('Record deleted succesfully!, Refresh');</script>";
+      header('refresh: 3; url=tables.php');
+    } else {
+      echo ("ERROR + $query") . pg_errormessage($query);
+    }
+    pg_close($connect);
+
+?>
