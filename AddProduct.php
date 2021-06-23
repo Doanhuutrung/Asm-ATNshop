75a5a0e1bacf8e0abcea24822cb7931154b7d5a2
BondDoan <68988327+Doanhuutrung@users.noreply.github.com>
2021-06-23 12:24:33 +0700

    Update AddProduct.php

--- AddProduct.php
+++ AddProduct.php
@@ -24,4 +24,24 @@
       echo ("ERROR + $query") . pg_errormessage($query);
     }
     pg_close($connect);
+
+        if ($connect === false) {
+      die("ERROR: Something went wrong with connection!");
+    } else {
+      $product_name = $_POST['Productname'];
+      $product_type = $_POST['Type'];
+      $price = $_POST['Price'];
+
+    }
+    $query = "INSERT INTO product (product_name, product_type, price) 
+    VALUES('$product_name', '$product_type', '$price');";
+    $result = pg_query($connect, $query);
+
+    if ($result) {
+      echo "<script>alert('Record added succesfully!, Refresh');</script>";
+      header('refresh: 3; url=tables.php');
+    } else {
+      echo ("ERROR + $query") . pg_errormessage($query);
+    }
+    pg_close($connect);
 ?>
