db03c72b02b7ef46dc7eee48a99b8e47a62dd53e
BondDoan <68988327+Doanhuutrung@users.noreply.github.com>
2021-06-30 10:01:16 +0700

    Add files via upload

---
+++ Del.php
@@ -0,0 +1,23 @@
+<?php
+   $connect = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com
+   dbname=d7fnvuie08bqea
+   port=5432
+   user=qbwkglenipljgh
+   password=bd2de50a1827c44d273e78356a3fca0f849403100e1a4b5f12d1c91f91a7e9ee
+    sslmode=require");
+     if ($connect === false) {
+      die("ERROR: Something went wrong with connection!");
+    } else {
+      $product_name = $_POST['Productname'];
+    }
+    $query = "DELETE FROM product 
+    Where product_name = '$product_name'" ;
+    $result = pg_query($connect, $query);
+    if ($result) {
+      echo "<script>alert('Record deleted succesfully!, Refresh');</script>";
+      header('refresh: 3; url=Add.php');
+    } else {
+      echo ("ERROR ");
+    }
+    pg_close($connect);
+?>
